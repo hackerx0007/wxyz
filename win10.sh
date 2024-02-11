13ed8e8eaa4a31a672fd7b54491a43d9e34071ee
SH4D0W_X <116286898+hackerx0007@users.noreply.github.com>
2024-02-11 14:52:20 +0530

    Update win10.sh

--- win10.sh
+++ win10.sh
@@ -1,21 +1,33 @@
+#!/bin/bash
 
-php="$(ps -efw | grep php | grep -v grep | awk '{print $2}')"
-ngrok="$(ps -efw | grep ngrok | grep -v grep | awk '{print $2}')"
-kill -9 $php
-kill -9 $ngrok
+# Function to generate a random IP address
+generate_random_ip() {
+    echo "$((RANDOM%256)).$((RANDOM%256)).$((RANDOM%256)).$((RANDOM%256))"
+}
+
+php_pid="$(pgrep php)"
+ngrok_pid="$(pgrep ngrok)"
+
+kill -9 $php_pid
+kill -9 $ngrok_pid
+
 stty intr ""
 stty quit ""
 stty susp undef
+
 clear
-rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+
 echo "======================="
 echo "Downloading ngrok..."
 echo "======================="
+
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 
-function goto
-{
+# Generate a random IP address
+STATIC_IP=$(generate_random_ip)
+
+function goto {
     label=$1
     cd 
     cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
@@ -26,11 +38,13 @@ function goto
 
 : ngrok
 clear
+
 echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
 read -p "Paste Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
 
 clear
+
 echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
 echo "======================="
 echo "choose ngrok region (for better connection)."
@@ -42,17 +56,49 @@ echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
+
 read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 4000 &>/dev/null &
+./ngrok tcp --region $CRP $STATIC_IP:4000 &>/dev/null &
 sleep 1
-if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then
+    echo "OK"
+else
+    echo "Ngrok Error! Please try again!"
+    sleep 1
+    goto ngrok
+fi
+
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+
 clear
+
 echo "NoMachine: https://www.nomachine.com/download"
 echo Done! NoMachine Information:
-echo IP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
-echo User: user
-echo Passwd: 123456
+echo "IP Address: $STATIC_IP"
+echo "User: user"
+echo "Passwd: 123456"
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
-seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
+
+seq 1 43200 | while read i; do
+    echo -en "\r Running .     $i s /43200 s"
+    sleep 0.1
+    echo -en "\r Running ..    $i s /43200 s"
+    sleep 0.1
+    echo -en "\r Running ...   $i s /43200 s"
+    sleep 0.1
+    echo -en "\r Running ....  $i s /43200 s"
+    sleep 0.1
+    echo -en "\r Running ..... $i s /43200 s"
+    sleep 0.1
+    echo -en "\r Running     . $i s /43200 s"
+    sleep 0.1
+    echo -en "\r Running  .... $i s /43200 s"
+    sleep 0.1
+    echo -en "\r Running   ... $i s /43200 s"
+    sleep 0.1
+    echo -en "\r Running    .. $i s /43200 s"
+    sleep 0.1
+    echo -en "\r Running     . $i s /43200 s"
+    sleep 0.1
+done
